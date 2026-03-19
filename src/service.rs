5acbb39e5a68cd9d55fb93612cbee81962e258eb
Dante Doménech <dante19031999@gmail.com>
2026-03-19 11:23:59 +0100

    Use modern rust module structure

--- src/service.rs
+++ src/service.rs
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026 Dante Doménech Martinez dante19031999@gmail.com
 
+#[cfg(feature = "aws")]
+pub mod aws;
 mod error;
 mod fallback;
 mod fv1hash;
+#[cfg(feature = "loki")]
+pub mod loki;
 pub(crate) mod serde;
 mod serror;
 mod service;
 pub mod write;
-#[cfg(feature = "loki")]
-pub mod loki;
-#[cfg(feature = "aws")]
-pub mod aws;
 
 pub use error::*;
 pub use fallback::*;
@@ -23,23 +23,23 @@ pub use serde::BasicAuth;
 pub use serror::*;
 pub use service::*;
 
-pub use crate::service::write::MessageFormatter as WriteMessageFormatter;
 pub use crate::service::write::DefaultMessageFormatter as DefaultWriteMessageFormatter;
+pub use crate::service::write::MessageFormatter as WriteMessageFormatter;
 
 pub use crate::service::write::BoxedIoService as BoxedIoWriteService;
 pub use crate::service::write::DefaultBoxedIoService as DefaultBoxedIoWriteService;
-pub use crate::service::write::DefaultFileWriteService as DefaultFileWriteService;
-pub use crate::service::write::FileWriteService as FileWriteService;
+pub use crate::service::write::DefaultFileWriteService;
+pub use crate::service::write::FileWriteService;
 pub use crate::service::write::IoService as IoWriteService;
 
+pub use crate::service::write::DefaultStringService as DefaultStringWriteService;
 pub use crate::service::write::FmtService as FmtWriteService;
 pub use crate::service::write::StringService as StringWriteService;
-pub use crate::service::write::DefaultStringService as DefaultStringWriteService;
 
-pub use crate::service::write::CoutService as CoutWriteService;
 pub use crate::service::write::CerrService as CerrWriteService;
-pub use crate::service::write::DefaultCoutService as DefaultCoutWriteService;
+pub use crate::service::write::CoutService as CoutWriteService;
 pub use crate::service::write::DefaultCerrService as DefaultCerrWriteService;
+pub use crate::service::write::DefaultCoutService as DefaultCoutWriteService;
 
 pub use crate::service::write::BoxedFmtService as BoxedFmtWriteService;
 pub use crate::service::write::DefaultBoxedFmtService as DefaultBoxedFmtWriteService;
@@ -49,20 +49,26 @@ pub use crate::service::write::DefaultArcedFmtService as DefaultArcedFmtWriteSer
 
 #[cfg(feature = "loki")]
 #[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
+pub use crate::service::loki::Config as LokiConfig;
+#[cfg(feature = "loki")]
+#[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
 pub use crate::service::loki::Data as LokiData;
+#[cfg(feature = "loki")]
+#[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
+pub use crate::service::loki::DefaultService as DefaultLokiService;
 #[cfg(feature = "loki")]
 #[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
 pub use crate::service::loki::Message as LokiMessage;
 #[cfg(feature = "loki")]
 #[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
-pub use crate::service::loki::Config as LokiConfig;
-#[cfg(feature = "loki")]
-#[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
 pub use crate::service::loki::Service as LokiService;
-#[cfg(feature = "loki")]
-#[cfg_attr(docsrs, doc(cfg(feature = "loki")))]
-pub use crate::service::loki::DefaultService as DefaultLokiService;
 
+#[cfg(feature = "aws")]
+#[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
+pub use crate::service::aws::CloudWatch;
+#[cfg(feature = "aws")]
+#[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
+pub use crate::service::aws::Config as CloudWatchConfig;
 #[cfg(feature = "aws")]
 #[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
 pub use crate::service::aws::Data as CloudWatchData;
@@ -71,10 +77,4 @@ pub use crate::service::aws::Data as CloudWatchData;
 pub use crate::service::aws::Message as CloudWatchMessage;
 #[cfg(feature = "aws")]
 #[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
-pub use crate::service::aws::Config as CloudWatchConfig;
-#[cfg(feature = "aws")]
-#[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
-pub use crate::service::aws::CloudWatch as CloudWatch;
-#[cfg(feature = "aws")]
-#[cfg_attr(docsrs, doc(cfg(feature = "aws")))]
-pub use crate::service::aws::SimpleCloudWatch as SimpleCloudWatch;
+pub use crate::service::aws::SimpleCloudWatch;
